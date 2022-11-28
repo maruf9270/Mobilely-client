8042005495ebff436bc49e3074384282c5bc98b5
Maruf9270 <Maruf9270@gmail.com>
2022-11-28 23:37:22 +0600

    Fixed a minor bug

--- src/Home/HomeLayout/HomePage/Components/ReportConfirmModal.jsx
+++ src/Home/HomeLayout/HomePage/Components/ReportConfirmModal.jsx
@@ -10,8 +10,8 @@ const ReportConfirmModal = ({handleReport,setReport}) => {
 <input type="checkbox" id="reportConfirm" className="modal-toggle" />
 <div className="modal modal-bottom sm:modal-middle">
   <div className="modal-box">
-    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <h3 className="font-bold text-lg">Report Confirmation</h3>
+    <p className="py-4">Are You sure you want to report about this item to admin</p>
     <div className="modal-action">
       <label htmlFor="reportConfirm" className="btn btn-primary" onClick={handleReport}>Confirm</label>
       <label htmlFor="reportConfirm" className="btn" onClick={()=>setReport(null)}>Cancel</label>
